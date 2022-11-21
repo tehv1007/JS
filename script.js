95c2d6e96eb113aaf3417b8426b9407eedc1eadb
tehv1007 <hanquoclythu@gmail.com>
2022-11-21 15:12:25 +0700

    Momentum App

---
+++ script.js
@@ -0,0 +1,77 @@
+const time = document.querySelector(".time");
+const greeting = document.querySelector(".greeting");
+const myName = document.querySelector(".my-name");
+const myFocus = document.querySelector(".my-focus");
+const body = document.body;
+
+function showTime() {
+  const date = new Date(); // constructor function with object, new operator
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+
+  //   time.innerHTML = hour + ":" + minute + ":" + second;
+  time.innerHTML = `${hour}:${addZero(minute)}:${addZero(second)}`;
+  setTimeout(showTime, 1000);
+}
+
+function addZero(number) {
+  return number < 10 ? "0" + number : number;
+}
+
+function showBackground() {
+  const hour = new Date().getHours();
+  if (hour < 12) {
+    body.style.backgroundImage = "url(./images/img1.jpg)";
+    body.style.backgroundSize = "cover";
+    body.style.backgroundPosition = "center";
+    greeting.innerHTML = "Good morning, ";
+  } else if (hour < 18) {
+    body.style.backgroundImage = "url(./images/img2.jpg)";
+    body.style.backgroundSize = "cover";
+    body.style.backgroundPosition = "center";
+    greeting.innerHTML = "Good afternoon, ";
+  } else {
+    body.style.backgroundImage = "url(./images/img3.jpg)";
+    body.style.backgroundSize = "cover";
+    body.style.backgroundPosition = "center";
+    greeting.innerHTML = "Good night, ";
+  }
+}
+
+// Name section
+function setName(event) {
+  if (event.key === "Enter") {
+    localStorage.setItem("name", event.target.innerHTML);
+    myName.blur();
+  }
+}
+
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    myName.innerHTML = "[Enter name]";
+  } else {
+    myName.innerHTML = localStorage.getItem("name");
+  }
+}
+
+// Focus section
+function setFocus(event) {
+  if (event.key === "Enter") {
+    localStorage.setItem("focus", event.target.innerHTML);
+    myFocus.blur();
+  }
+}
+
+function getFocus() {
+  if (!localStorage.getItem("focus")) myFocus.innerHTML = "[Enter your Focus]";
+  else myFocus.innerHTML = localStorage.getItem("focus");
+}
+
+myName.addEventListener("keydown", setName);
+myFocus.addEventListener("keydown", setFocus);
+
+showTime();
+showBackground();
+getName();
+getFocus();
